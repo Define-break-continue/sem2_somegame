8489b1844e9059540f43a038788361bfc9fdd9c3
bagrusss <bagrusss@gmail.com>
2015-09-23 15:36:47 +0300

    server from ex

---
+++ Java_server/src/main/java/example/Main.java
@@ -0,0 +1,40 @@
+package example;
+
+import admin.AdminPageServlet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.jetbrains.annotations.NotNull;
+
+public class Main {
+
+    private static final String START_MESSAGE="Starting at port: ";
+
+    public static void main(@NotNull String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.append("Use port as the first argument");
+            System.exit(1);
+        }
+        String portString = args[0];
+        int port = Integer.valueOf(portString);
+        System.out.append(START_MESSAGE).append(portString).append('\n');
+
+        Server server = new Server(port);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new AdminPageServlet()), AdminPageServlet.ADMIN_PAGE_URL);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("static");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+}
---
+++ Java_server/src/main/java/ru/bagrusss/models/UserModel.java
@@ -0,0 +1,35 @@
+package ru.bagrusss.models;
+
+/**
+ * Created by vladislav.
+ */
+
+public class UserModel {
+    private String userPassword;
+    private String userLogin;
+    private String userEmail;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+}
