f6eae60c4ccbbf607154f27fcf63661cce0ea42e
bagrusss <bagrusss@gmail.com>
2015-09-30 16:50:59 +0300

    Навел порядок в проекте

--- Java_server/start.sh
+++
@@ -1,1 +0,0 @@
-java -classpath Java_server.jar Main 8080
--- src/main/java/rus/bagrusss/templater/PageGenerator.java
+++ src/main/java/rus/bagrusss/templater/PageGenerator.java
@@ -3,6 +3,7 @@ package rus.bagrusss.templater;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
@@ -14,14 +15,15 @@ import java.util.Map;
 /**
  * @author v.chibrikov
  */
+
 public class PageGenerator {
 
-    private static final String HTML_DIR = "tml";
+    private static final String HTML_DIR = "server_tml";
     private static final Configuration SERVER_CONFIGS = new Configuration();
 
     @Nullable
     @SuppressWarnings("ObjectToString")
-    public static String getPage(String filename, @Nullable Map<String, Object> data) {
+    public static String getPage(@NotNull String filename, @Nullable Map<String, Object> data) {
         Writer stream = new StringWriter();
         try {
             Template template = SERVER_CONFIGS != null ? SERVER_CONFIGS.getTemplate(HTML_DIR + File.separator + filename) : null;
